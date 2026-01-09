34e953fa1b5ee549c6d63b0187385e93174531a4
Sharma <tesharma2@gmail.com>
2026-01-09 07:59:56 +0100

    sharma

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -2,9 +2,13 @@
 
 export const CONTRACT_ADDRESSES = {
   amoy: {
-    TokenizedCurrency: "0xdb204732615f1EC2bDb1Aae2032bC9DE7aA8c164",
-    PriceOracle: "0x8063A3901b9053f911fFE3da4bAF754B640A0744",
-    TradingPlatform: "0xE13B97E70AF997dEaB3EAa28Ab88cCd362734729",
+    TokenizedCurrency: "0xAF430b788e47d43bB21083b999151FFDEF528a80",
+    PriceOracle: "0x129A60D49907934C64633Fa461708fa79b4232CD",
+    TradingPlatform: "0x8328329b9172514924dFa966D6c06c48C3BF3C5C",
   },
   // add polygon mainnet addresses later
+
+  // V2 Contracts
+  PriceOracleV2: "0x5e6038c073B8EB7d7b03Cc56503006183fe64eA1",
+  TradingPlatformV2: "0x735C78c95da6284244771F66B5aA4c0BE38fb7c7",
 };
