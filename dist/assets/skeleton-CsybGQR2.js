35b6b03553bfe8e435cbc718a29ce63160d7a085
Sharma <tesharma2@gmail.com>
2026-09-19 17:46:39 +0200

    Update production build

---
+++ dist/assets/skeleton-CsybGQR2.js
@@ -0,0 +1,1 @@
+import{j as m}from"./react-CWuWlbEf.js";import{x as o}from"./index-C2zKsylc.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
